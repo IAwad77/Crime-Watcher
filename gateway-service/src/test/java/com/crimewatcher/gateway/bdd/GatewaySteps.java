f13c9da2bb76ecc87406dcf241d58547390f90c9
Your Name <your@email>
2025-05-04 10:00:00 -0500

    Sprint 4 Gateway-Service

---
+++ gateway-service/src/test/java/com/crimewatcher/gateway/bdd/GatewaySteps.java
@@ -0,0 +1,47 @@
+﻿package com.crimewatcher.gateway.bdd;
+
+import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GatewaySteps {
+
+    private WebDriver driver;
+
+    @Given("the gateway test page is open")
+    public void open_page() {
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+        driver.manage()
+              .timeouts()
+              .implicitlyWait(Duration.ofSeconds(4));
+
+        String path = System.getProperty("user.dir")
+                       + "/src/test/resources/gateway.html";
+
+        driver.get("file:///" + path);
+    }
+
+    @When("I click load users")
+    public void click_button() {
+        driver.findElement(By.id("b")).click();
+    }
+
+    @Then("results contain '['")
+    public void check_response() throws InterruptedException {
+
+        Thread.sleep(1000);
+
+        String txt = driver.findElement(By.id("out")).getText();
+        assertTrue(txt.contains("["));
+
+        driver.quit();
+    }
+}
---
+++ gateway-service/src/test/resources/features/gateway.feature
@@ -0,0 +1,6 @@
+﻿Feature: Gateway routing
+
+  Scenario: User list flows through gateway
+    Given the gateway test page is open
+    When I click load users
+    Then results contain '['
