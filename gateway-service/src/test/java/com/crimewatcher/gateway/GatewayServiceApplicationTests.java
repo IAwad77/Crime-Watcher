7b3ec683f915b5ffa8c0c4b1fbc6ecfb006ce0ff
Your Name <your@email>
2025-07-03 01:19:37 -0500

    Sprint 4 Gateway-Service

---
+++ gateway-service/src/test/java/com/crimewatcher/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+﻿package com.crimewatcher.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() { }
+}
